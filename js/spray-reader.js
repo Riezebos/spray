7664c85aee7370a6b35805d3fc6a6373e315a734
Simon Riezebos <simon.riezebos@enexis.nl>
2025-04-29 15:32:05 +0200

    Add pause button

--- js/spray-reader.js
+++ js/spray-reader.js
@@ -1,4 +1,4 @@
-var SprayReader = function(container){
+var SprayReader = function (container) {
   this.container = $(container);
 };
 SprayReader.prototype = {
@@ -9,40 +9,41 @@ SprayReader.prototype = {
   input: null,
   words: null,
   isRunning: false,
+  isPaused: false,
   timers: [],
-  
-  setInput: function(input) {
+
+  setInput: function (input) {
     this.input = input;
-    
+
     // Split on spaces
     var allWords = input.split(/\s+/);
-    
+
     var word = '';
     var result = '';
-    
+
     // Preprocess words
     var tmpWords = allWords.slice(0); // copy Array
     var t = 0;
 
-    for (var i=0; i<allWords.length; i++){
+    for (var i = 0; i < allWords.length; i++) {
 
-      if(allWords[i].indexOf('.') != -1){
+      if (allWords[i].indexOf('.') != -1) {
         tmpWords[t] = allWords[i].replace('.', '•');
       }
 
       // Double up on long words and words with commas.
-      if((allWords[i].indexOf(',') != -1 || allWords[i].indexOf(':') != -1 || allWords[i].indexOf('-') != -1 || allWords[i].indexOf('(') != -1|| allWords[i].length > 8) && allWords[i].indexOf('.') == -1){
-        tmpWords.splice(t+1, 0, allWords[i]);
-        tmpWords.splice(t+1, 0, allWords[i]);
+      if ((allWords[i].indexOf(',') != -1 || allWords[i].indexOf(':') != -1 || allWords[i].indexOf('-') != -1 || allWords[i].indexOf('(') != -1 || allWords[i].length > 8) && allWords[i].indexOf('.') == -1) {
+        tmpWords.splice(t + 1, 0, allWords[i]);
+        tmpWords.splice(t + 1, 0, allWords[i]);
         t++;
         t++;
       }
 
       // Add an additional space after punctuation.
-      if(allWords[i].indexOf('.') != -1 || allWords[i].indexOf('!') != -1 || allWords[i].indexOf('?') != -1 || allWords[i].indexOf(':') != -1 || allWords[i].indexOf(';') != -1|| allWords[i].indexOf(')') != -1){
-        tmpWords.splice(t+1, 0, ".");
-        tmpWords.splice(t+1, 0, ".");
-        tmpWords.splice(t+1, 0, ".");
+      if (allWords[i].indexOf('.') != -1 || allWords[i].indexOf('!') != -1 || allWords[i].indexOf('?') != -1 || allWords[i].indexOf(':') != -1 || allWords[i].indexOf(';') != -1 || allWords[i].indexOf(')') != -1) {
+        tmpWords.splice(t + 1, 0, ".");
+        tmpWords.splice(t + 1, 0, ".");
+        tmpWords.splice(t + 1, 0, ".");
         t++;
         t++;
         t++;
@@ -53,41 +54,78 @@ SprayReader.prototype = {
 
     this.words = tmpWords.slice(0);
     this.wordIdx = 0;
+    this.isPaused = false; // Reset pause state on new input
   },
-  
-  setWpm: function(wpm) {
+
+  setWpm: function (wpm) {
     this.wpm = parseInt(wpm, 10);
-    this.msPerWord = 60000/wpm;
+    this.msPerWord = 60000 / wpm;
   },
-  
-  start: function() {
+
+  start: function () {
+    // Don't start if already running
+    if (this.isRunning) {
+      return;
+    }
+
+    // If paused, just clear the pause state and restart the timer
+    if (this.isPaused) {
+      this.isPaused = false;
+      this.isRunning = true;
+
+      thisObj = this; // Ensure thisObj is set
+      this.timers.push(setInterval(function () {
+        thisObj.displayWordAndIncrement();
+      }, this.msPerWord));
+      return;
+    }
+
+    // If not paused and not running, start from the beginning (or current index if applicable)
     this.isRunning = true;
-    
+    this.isPaused = false;
+
     thisObj = this;
-    
-    this.timers.push(setInterval(function() {
+
+    this.timers.push(setInterval(function () {
       thisObj.displayWordAndIncrement();
     }, this.msPerWord));
   },
-  
-  stop: function() {
+
+  stop: function () {
     this.isRunning = false;
-    
-    for(var i = 0; i < this.timers.length; i++) {
+    this.isPaused = false;
+    this.wordIdx = 0; // Reset word index on stop
+
+    for (var i = 0; i < this.timers.length; i++) {
       clearTimeout(this.timers[i]);
     }
+    this.timers = []; // Clear timers array
+    this.container.html('&nbsp;'); // Clear display on stop
   },
-  
-  displayWordAndIncrement: function() {
+
+  pause: function () {
+    if (!this.isRunning) {
+      return;
+    }
+    this.isRunning = false;
+    this.isPaused = true;
+
+    for (var i = 0; i < this.timers.length; i++) {
+      clearTimeout(this.timers[i]);
+    }
+    this.timers = []; // Clear timers array
+  },
+
+  displayWordAndIncrement: function () {
     var pivotedWord = pivot(this.words[this.wordIdx]);
-  
+
     this.container.html(pivotedWord);
-    
+
     this.wordIdx++;
     if (thisObj.wordIdx >= thisObj.words.length) {
       this.wordIdx = 0;
       this.stop();
-      if(typeof(this.afterDoneCallback) === 'function') {
+      if (typeof (this.afterDoneCallback) === 'function') {
         this.afterDoneCallback();
       }
     }
@@ -95,71 +133,71 @@ SprayReader.prototype = {
 };
 
 // Find the red-character of the current word.
-function pivot(word){
-    var length = word.length;
+function pivot(word) {
+  var length = word.length;
 
-    // Longer words are "right-weighted" for easier readability.
-    if(length<6){
+  // Longer words are "right-weighted" for easier readability.
+  if (length < 6) {
 
-        var bit = 1;
-        while(word.length < 22){
-            if(bit > 0){
-                word = word + '.';
-            }
-            else{
-                word = '.' + word;
-            }
-            bit = bit * -1;
-        }
-
-        var start = '';
-        var end = '';
-        if((length % 2) === 0){
-            start = word.slice(0, word.length/2);
-            end = word.slice(word.length/2, word.length);
-        } else{
-            start = word.slice(0, word.length/2);
-            end = word.slice(word.length/2, word.length);
-        }
-
-        var result;
-        result = "<span class='spray_start'>" + start.slice(0, start.length -1);
-        result = result + "</span><span class='spray_pivot'>";
-        result = result + start.slice(start.length-1, start.length);
-        result = result + "</span><span class='spray_end'>";
-        result = result + end;
-        result = result + "</span>";
+    var bit = 1;
+    while (word.length < 22) {
+      if (bit > 0) {
+        word = word + '.';
+      }
+      else {
+        word = '.' + word;
+      }
+      bit = bit * -1;
     }
 
-    else{
-
-        word = '.......' + word;
-        
-        var tail = 22 - (word.length + 7);
-        if(tail > 0) {
-          word + ('.'.repeat(tail));
-        }
-
-        var start = word.slice(0, word.length/2);
-        var end = word.slice(word.length/2, word.length);
-
-        var result;
-        result = "<span class='spray_start'>" + start.slice(0, start.length -1);
-        result = result + "</span><span class='spray_pivot'>";
-        result = result + start.slice(start.length-1, start.length);
-        result = result + "</span><span class='spray_end'>";
-        result = result + end;
-        result = result + "</span>";
-
+    var start = '';
+    var end = '';
+    if ((length % 2) === 0) {
+      start = word.slice(0, word.length / 2);
+      end = word.slice(word.length / 2, word.length);
+    } else {
+      start = word.slice(0, word.length / 2);
+      end = word.slice(word.length / 2, word.length);
     }
 
-    result = result.replace(/\./g, "<span class='invisible'>.</span>");
+    var result;
+    result = "<span class='spray_start'>" + start.slice(0, start.length - 1);
+    result = result + "</span><span class='spray_pivot'>";
+    result = result + start.slice(start.length - 1, start.length);
+    result = result + "</span><span class='spray_end'>";
+    result = result + end;
+    result = result + "</span>";
+  }
 
-    return result;
+  else {
+
+    word = '.......' + word;
+
+    var tail = 22 - (word.length + 7);
+    if (tail > 0) {
+      word + ('.'.repeat(tail));
+    }
+
+    var start = word.slice(0, word.length / 2);
+    var end = word.slice(word.length / 2, word.length);
+
+    var result;
+    result = "<span class='spray_start'>" + start.slice(0, start.length - 1);
+    result = result + "</span><span class='spray_pivot'>";
+    result = result + start.slice(start.length - 1, start.length);
+    result = result + "</span><span class='spray_end'>";
+    result = result + end;
+    result = result + "</span>";
+
+  }
+
+  result = result.replace(/\./g, "<span class='invisible'>.</span>");
+
+  return result;
 }
 
 // Let strings repeat themselves,
 // because JavaScript isn't as awesome as Python.
-String.prototype.repeat = function( num ){
-    return new Array( num + 1 ).join( this );
+String.prototype.repeat = function (num) {
+  return new Array(num + 1).join(this);
 }
